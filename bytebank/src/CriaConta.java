0cba8f99dbaa767a47295d058b04617df5d50b30
L F <franco100lucas@gmail.com>
2023-06-11 17:16:15 -0300

    Iniciando Java

---
+++ bytebank/src/CriaConta.java
@@ -0,0 +1,10 @@
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		Conta primeiraConta= new Conta(); 
+		//Instânciando e criando um objeto do tipo conta
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+	}
+}
