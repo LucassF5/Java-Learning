61f98e06523445cddce3d0a0cec6198f681e8c55
L F <franco100lucas@gmail.com>
2023-06-21 05:06:22 -0300

    Utilizando Herança

---
+++ bytebank-herdado/src/Gerente.java
@@ -0,0 +1,26 @@
+
+//Gerente é um Funcionario, Gerente herda da classe Funcionario
+public class Gerente extends Funcionario {
+	//extends serve para a classe herdar métodos de outra
+	
+	private int senha;
+	
+	
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	/*public double getBonificacao() {
+		return this.salario;
+	}*/
+	
+}
---
+++ bytebank-herdado/src/TesteGerente.java
@@ -0,0 +1,21 @@
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+		Gerente g1 = new Gerente();
+		g1.setNome("Lucas");
+		g1.setCpf("4322541");
+		g1.setSalario(5000.0);
+		
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
+		
+		g1.setSenha(23445);
+		boolean autenticou = g1.autentica(23445);
+		
+		System.out.println(autenticou);
+		
+	}
+
+}
