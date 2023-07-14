98d8924a1c9a57db9bd45c098a27db5f1efa6c2a
L F <franco100lucas@gmail.com>
2023-07-14 05:29:38 -0300

    Exceção checked e unchecked

--- Java-pilha/src/Fluxo.java
+++ Java-pilha/src/Fluxo.java
@@ -3,6 +3,7 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Início do main");
+//        Não é necessário colocar "throws MinhaException" pois há um bloco try catch
         try {
         	metodo1();        	            	
         } catch(ArithmeticException | NullPointerException | MinhaException ex) {
@@ -13,15 +14,26 @@ public class Fluxo {
         	System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MinhaException {
+//    	Usando throws pois MinhaException é checked, herda diretamente de Exception
         System.out.println("Início do metodo1");
+        
         metodo2();
+        
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MinhaException {
         System.out.println("Início do metodo2");
-        throw new MinhaException("Deu errado");
+        new MinhaException("Deu errado");
 //        System.out.println("Fim do metodo2");
     }
 }
+
+// MinhaException faz parte da categoria checked e ela é necessária explicitar na assinatura do método
+// Nas de tipo unchecked não é necessário
+// Checked são chamadas de "checked" (checadas) porque o compilador verifica 
+// se essas exceções foram tratadas ou se foram lançadas novamente usando o comando "throws"
+
+//a primeira obriga o desenvolvedor a tratá-la(checked), isto é, usar umas das duas formas básicas de tratamento
+// de exceção no Java: captura ou tratamento(try catch); enquanto a segunda exceção não o faz
