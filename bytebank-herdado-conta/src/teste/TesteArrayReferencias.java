bd6f8b3fc03234384c7fc3d4ec9f07285b0113eb
L F <franco100lucas@gmail.com>
2023-07-29 00:35:11 -0300

    Guardando referências em um array

--- bytebank-herdado-conta/src/teste/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/teste/TesteArrayReferencias.java
@@ -1,20 +1,29 @@
 package teste;
 
+import modelo.Cliente;
 import modelo.Conta;
 import modelo.ContaCorrente;
+import modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 
-		ContaCorrente contas [] = new ContaCorrente [5];
+		Object referencias [] = new Object [5];
+		
 		ContaCorrente cc1 = new ContaCorrente(10, 1003);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
 		ContaCorrente cc2 = new ContaCorrente(23, 1054);
-		contas[1] = cc2;
+		referencias[1] = cc2;
 		
-		System.out.println(contas[0].getNumero());
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
+		
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast, mudando o tipo
+//		Casting é transformar uma referência genérica em uma específica
+		// System.out.println(referencias[0].getNumero());
+		System.out.println(ref.getAgencia());
 		
 	}
 
