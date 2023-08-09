064a9b2219c7a7bb4e3eac919d222d3a20eaecbf
L F <franco100lucas@gmail.com>
2023-08-09 10:37:37 -0300

    Utilizando e aprendendo mais autoboxing e unboxing

--- bytebank-herdado-conta/src/teste/util/Teste.java
+++ bytebank-herdado-conta/src/teste/util/Teste.java
@@ -11,9 +11,21 @@ public class Teste {
                 String[] nomes = new String[5];
 
                 int idade = 29;//Integer
-                Integer idadeRef = new Integer(29); // tipo int vira Integer(Classe) - Wrapper
+                Integer idadeRef = Integer.valueOf(idade); // Autoboxing
+                // tipo int vira Integer(Classe) - Wrapper 
 				// Transforma o primitivo em objeto, e guarda a referência no array
 				// Wrapper - Classe que representa um tipo primitivo e embrulha, transformando em um objeto
+                
+                System.out.println(idadeRef.doubleValue());
+                
+                int valor = idadeRef.intValue(); //Unboxing
+                
+                String s = args[0];
+                
+//              Integer numero = Integer.valueOf(s);
+                int numero = Integer.parseInt(s); // parseInt já transforma a Str em Int	
+                System.out.println(numero);
+                
                 List<Integer> numeros = new ArrayList<Integer>();
                 numeros.add(29); //Autoboxing
 				// Java automaticamente pega o primitivo e cria em um objeto, logo após guarda a referência no array
