6f4ab46824b5394f3f9fe99c026642453aaf2c41
L F <franco100lucas@gmail.com>
2023-08-12 00:07:50 -0300

    Utilizando funções lambda

--- bytebank-herdado-conta/src/teste/util/TesteLambda.java
+++ bytebank-herdado-conta/src/teste/util/TesteLambda.java
@@ -9,31 +9,31 @@ import modelo.Conta;
 import modelo.ContaCorrente;
 import modelo.ContaPoupanca;
 
-public class Teste {
+public class TesteLambda {
 
 	public static void main(String[] args) {
 		
 		Conta cc1 = new ContaCorrente(22, 33);
 		Cliente clienteCC1 = new Cliente();
-		clienteCC1.setNome("Nico");
+		clienteCC1.setNome("Lucas");
 		cc1.setTitular(clienteCC1);
 		cc1.deposita(333.0);
 		
 		Conta cc2 = new ContaPoupanca(22, 44);
 		Cliente clienteCC2 = new Cliente();
-		clienteCC2.setNome("Guilherme");
+		clienteCC2.setNome("Ingrid");
 		cc2.setTitular(clienteCC2);
 		cc2.deposita(444.0);
 		
 		Conta cc3 = new ContaCorrente(22, 11);
 		Cliente clienteCC3 = new Cliente();
-		clienteCC3.setNome("Paulo");
+		clienteCC3.setNome("Maria");
 		cc3.setTitular(clienteCC3);
 		cc3.deposita(111.0);
 		
 		Conta cc4 = new ContaPoupanca(22, 22);
 		Cliente clienteCC4 = new Cliente();
-		clienteCC4.setNome("Ana");
+		clienteCC4.setNome("Francisca");
 		cc4.setTitular(clienteCC4);
 		cc4.deposita(222.0);
 
@@ -44,29 +44,19 @@ public class Teste {
 		lista.add(cc4);
 		
 //		Classes anônimas só são implementadas no local, sem precisar ser usadas em outros lugares
-		lista.sort(new Comparator<Conta>() { //classe anônima
+		lista.sort((c1, c2) -> Integer.compare(c1.getNumero(), c2.getNumero()) ); // -> indica função lambda
 
-	        @Override
-	        public int compare(Conta c1, Conta c2) {
-
-	            return Integer.compare(c1.getNumero(), c2.getNumero());
-	                    }
-	                }
-	                );
-
-	                Comparator<Conta> comp = new Comparator<Conta>() {
-
-	                        @Override
-	                        public int compare(Conta c1, Conta c2) {
-	                            String nomeC1 = c1.getTitular().getNome();
-	                            String nomeC2 = c2.getTitular().getNome();
-	                            return nomeC1.compareTo(nomeC2);
-	                        }
-	                };
-
-	        for (Conta conta : lista) {
-	                System.out.println(conta + ", " + conta.getTitular().getNome());
-	        }
+		
+		Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+			String nomeC1 = c1.getTitular().getNome();
+			String nomeC2 = c2.getTitular().getNome();
+			return nomeC1.compareTo(nomeC2);
+		};
+		
+		lista.sort(comp);
+		
+		lista.forEach((Conta conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()) );
+		
 	}
 }
 	
